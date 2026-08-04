20d6b5149b015384274aaa1907db0db8e066e380
WOjkurt <kurtwojtyle.rizal@dbtc-cebu.edu.ph>
2026-08-04 09:33:01 +0800

    feat: complete Angular reusable components activity (Flags 0-10)

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { ProfileCard } from './components/profile-card/profile-card';
+import { AnnouncementCard } from './components/announcement-card/announcement-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    Footer,
+    ProfileCard,
+    AnnouncementCard
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

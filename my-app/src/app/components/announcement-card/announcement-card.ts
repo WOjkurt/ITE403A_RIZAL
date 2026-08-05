15f984cd16718c95e7885662ebef3f3c1a3eda84
WOjkurt <kurtwojtyle.rizal@dbtc-cebu.edu.ph>
2026-08-05 10:38:43 +0800

    Activity 3: Interpolation

--- my-app/src/app/components/announcement-card/announcement-card.ts
+++ my-app/src/app/components/announcement-card/announcement-card.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './announcement-card.html',
   styleUrl: './announcement-card.css',
 })
-export class AnnouncementCard {}
+export class AnnouncementCard {
+  AnnouncementTitle1:string = "AnnouncementTitle1";
+  AnnouncementBody1:string = "AnnouncementBody1";
+
+  AnnouncementTitle2:string = "AnnouncementTitle2";
+  AnnouncementBody2:string = "AnnouncementBody2";
+
+  AnnouncementTitle3:string = "AnnouncementTitle3";
+  AnnouncementBody3:string = "AnnouncementBody3";
+
+  AnnouncementTitle4:string = "AnnouncementTitle4";
+  AnnouncementBody4:string = "AnnouncementBody4";
+
+  AnnouncementTitle5:string = "AnnouncementTitle5";
+  AnnouncementBody5:string = "AnnouncementBody5";
+  
+  AnnouncementTitle6:string = "AnnouncementTitle6";
+  AnnouncementBody6:string = "AnnouncementBody6";
+
+  AnnouncementTitle7:string = "AnnouncementTitle7";
+  AnnouncementBody7:string = "AnnouncementBody7";
+
+  AnnouncementTitle8:string = "AnnouncementTitle8";
+  AnnouncementBody8:string = "AnnouncementBody8";
+  
+  AnnouncementTitle9:string = "AnnouncementTitle9";
+  AnnouncementBody9:string = "AnnouncementBody9";
+
+   AnnouncementTitle10:string = "AnnouncementTitle10";
+  AnnouncementBody10:string = "AnnouncementBody10";
+
+
+}
--- my-app/src/app/components/profile-card/profile-card.ts
+++ my-app/src/app/components/profile-card/profile-card.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.css',
+
 })
-export class ProfileCard {}
+export class ProfileCard {
+  StudentName:string = "Kurt Wojtyle S. Rizal";
+  StudentID =23017245
+  Course:string = "BSIT";
+  Year:string = "IV";
+  Section:string = "Undefined";
+  Nickname:string = "kurt";
+  Username:string = "kurtwojtyle.rizal";
+  Domain:string = "@dbtc-cebu.edu.ph";
+  Email: string = `${this.Username}${this.Domain}`;
+  Grade = 1.0;
+
+ 
+  
+ }
